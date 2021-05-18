cb5c44bdaee5af5e687b61424d967e54abbb9c51
yogianantaputra <yogi090891@gmail.com>
2021-05-18 08:28:56 +0700

    add local db

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def admin_only(f):
     return decorated_function
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///blog.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
